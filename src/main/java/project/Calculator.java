e3a4b9793ddb55a1da5e57666cdc1b11307dc9fb
Emad Ali <mc.lilzee@hotmail.com>
2022-10-06 00:33:22 +0200

    Add layout for buttons

--- src/main/java/project/Calculator.java
+++ src/main/java/project/Calculator.java
@@ -1,33 +1,76 @@
 package project;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class Calculator extends JFrame {
 
+    private final JTextField textField;
+
     public Calculator(String title) {
         super(title);
         setSize(400, 400);
+        setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.setBorder(new EmptyBorder(10, 20, 10, 20));
+        panel.setMaximumSize(new Dimension(getWidth(), 50));
 
-        JTextField field = new JTextField("2+2", 20);
-        field.setMaximumSize(new Dimension(150, 25));
-        field.setName("EquationTextField");
-        field.setAlignmentX(CENTER_ALIGNMENT);
+        textField = new JTextField();
+        textField.setName("EquationTextField");
+        textField.setAlignmentX(CENTER_ALIGNMENT);
+        textField.setFont(new Font(Font.MONOSPACED, Font.BOLD, 25));
 
-        JButton button = new JButton("Solve");
-        button.setName("Solve");
-        button.addActionListener(e -> field.setText("2+2=4"));
-        button.setAlignmentX(CENTER_ALIGNMENT);
+        panel.add(textField, BorderLayout.CENTER);
 
-        add(Box.createGlue());
-        add(field);
-        add(Box.createGlue());
-        add(button);
-        add(Box.createGlue());
+        add(panel);
+        add(createButtonsPanel());
 
         setVisible(true);
 
+    }
+
+    private JPanel createButtonsPanel() {
+        JPanel panel = new JPanel();
+        GridLayout layout = new GridLayout(4, 4);
+        layout.setHgap(20);
+        layout.setVgap(20);
+        panel.setLayout(layout);
+        panel.setBorder(new EmptyBorder(0,20, 20, 20));
+
+        panel.add(createCalculatorButton("7", "Seven"));
+        panel.add(createCalculatorButton("8", "Eight"));
+        panel.add(createCalculatorButton("9", "Nine"));
+        panel.add(createCalculatorButton("/", "Divide"));
+        panel.add(createCalculatorButton("4", "Four"));
+        panel.add(createCalculatorButton("5", "Five"));
+        panel.add(createCalculatorButton("6", "Six"));
+        panel.add(createCalculatorButton("x", "Multiply"));
+        panel.add(createCalculatorButton("1", "One"));
+        panel.add(createCalculatorButton("2", "Two"));
+        panel.add(createCalculatorButton("3", "Three"));
+        panel.add(createCalculatorButton("+", "Add"));
+        panel.add(Box.createGlue());
+        panel.add(createCalculatorButton("0", "Zero"));
+        panel.add(createCalculatorButton("=", "Equals"));
+        panel.add(createCalculatorButton("-", "Subtract"));
+        return panel;
+    }
+
+    private JButton createCalculatorButton(String value, String name) {
+        JButton button = new JButton(value);
+        button.setName(name);
+        attachActionListenerToButton(button);
+        return button;
+    }
+
+    private void attachActionListenerToButton(JButton button) {
+        button.addActionListener(e -> {
+            String fieldText = textField.getText();
+            textField.setText(fieldText + button.getText());
+        });
     }
 }
