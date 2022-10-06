e0f9134058fa993f2fbf7815782af711dddbaf8e
Emad Ali <mc.lilzee@hotmail.com>
2022-10-06 17:13:32 +0200

    Refactor CalculatorButton to be public class and remove NumericButton

--- src/main/java/project/CalculatorButton.java
+++ src/main/java/project/CalculatorButton.java
@@ -2,12 +2,16 @@ package project;
 
 import javax.swing.*;
 
-public abstract class CalculatorButton extends JButton {
+public class CalculatorButton extends JButton {
 
     public CalculatorButton(String text, String name) {
         super(text);
         this.setName(name);
     }
 
-    abstract void performOperation();
+    public void performOperation() {
+        addActionListener(e -> {
+            CalculatorScreen.buttonPress(getText());
+        });
+    }
 }
