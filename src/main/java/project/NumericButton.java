c8984f341c3a365b24e560966981a3c7525ec1fa
Emad Ali <mc.lilzee@hotmail.com>
2022-10-06 16:53:46 +0200

    Create NumericButton class

---
+++ src/main/java/project/NumericButton.java
@@ -0,0 +1,16 @@
+package project;
+
+public class NumericButton extends CalculatorButton {
+
+    public NumericButton(String text, String name) {
+        super(text, name);
+    }
+
+    @Override
+    void performOperation() {
+        addActionListener(e -> {
+
+        });
+    }
+
+}
